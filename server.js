4f323348fc146015ad54a4d1037c6595366b04e0
Rajath <rajath37@gmail.com>
2020-10-15 08:23:32 +0530

    Json web token

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express=require('express')
-const app=express();
+const connectDB=require('./config/db')
+
+
+const app=express(); 
+//Connect the Database 
+connectDB();
+
+// init middleware
+app.use(express.json({extended:false}))
+
 
 app.get('/',(req,res)=>res.send('API running'))
-
+// define routes
+app.use('/api/users',require('./config/routes/api/users')); 
+app.use('/api/post',require('./config/routes/api/post'));
+app.use('/api/profile',require('./config/routes/api/profile'));
+app.use('/api/auth',require('./config/routes/api/auth'));
 
 const PORT =process.env.PORT|| 5000; 
 app.listen(PORT,() => console.log(`Server started on Port ${PORT}`));
